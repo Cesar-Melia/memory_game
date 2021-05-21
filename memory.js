2015476de6ecd57392995f9eac39e81107a6e96b
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-05-21 07:49:58 +0200

    images

--- memory.js
+++ memory.js
@@ -1,63 +1,63 @@
 const cardArray = [
     {
         id: 1,
-        name: 'earth',
-        img: 'public/exercise-1/earth.svg'
+        name: "earth",
+        img: "images/earth.svg"
     },
     {
         id: 2,
-        name: 'jupiter',
-        img: 'public/exercise-1/jupiter.svg'
+        name: "jupiter",
+        img: "images/jupiter.svg"
     },
     {
         id: 3,
-        name: 'mars',
-        img: 'public/exercise-1/mars.svg'
+        name: "mars",
+        img: "images/mars.svg"
     },
     {
         id: 4,
-        name: 'mercury',
-        img: 'public/exercise-1/mercury.svg'
+        name: "mercury",
+        img: "images/mercury.svg"
     },
     {
         id: 5,
-        name: 'saturn',
-        img: 'public/exercise-1/saturn.svg'
+        name: "saturn",
+        img: "images/saturn.svg"
     },
     {
         id: 6,
-        name: 'uranus',
-        img: 'public/exercise-1/uranus.svg'
+        name: "uranus",
+        img: "images/uranus.svg"
     },
     {
         id: 7,
-        name: 'earth',
-        img: 'public/exercise-1/earth.svg'
+        name: "earth",
+        img: "images/earth.svg"
     },
     {
         id: 8,
-        name: 'jupiter',
-        img: 'public/exercise-1/jupiter.svg'
+        name: "jupiter",
+        img: "images/jupiter.svg"
     },
     {
         id: 9,
-        name: 'mars',
-        img: 'public/exercise-1/mars.svg'
+        name: "mars",
+        img: "images/mars.svg"
     },
     {
         id: 10,
-        name: 'mercury',
-        img: 'public/exercise-1/mercury.svg'
+        name: "mercury",
+        img: "images/mercury.svg"
     },
     {
         id: 11,
-        name: 'saturn',
-        img: 'public/exercise-1/saturn.svg'
+        name: "saturn",
+        img: "images/saturn.svg"
     },
     {
         id: 12,
-        name: 'uranus',
-        img: 'public/exercise-1/uranus.svg'
+        name: "uranus",
+        img: "images/uranus.svg"
     }
 ]
 
@@ -67,7 +67,7 @@ const div$$ = document.querySelector("[data-function='grid']");
 
 for (let i = 0; i < cardArray.length; i++){
     const img$$ = document.createElement("img");
-    img$$.setAttribute("src", "public/exercise-1/universe.svg");
+    img$$.setAttribute("src", "images/universe.svg");
     img$$.setAttribute("id", cardArray[i].id);
     img$$.setAttribute("name", cardArray[i].name);
     div$$.appendChild(img$$);
